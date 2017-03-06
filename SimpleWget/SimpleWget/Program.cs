434f601381ba10e3b1c26e33e9fd970097f31465
xinghc <xinghc@gmx.us>
2017-03-06 23:54:38 +0800

    Simple wget example

---
+++ SimpleWget/SimpleWget/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cli = new WebClient();
+            string data = cli.DownloadString("https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data");
+            Console.WriteLine(data);
+            Console.ReadKey();
+        }
+    }
+}
