33286e441cc72f849ea10f8fdb0b69891fff1e14
xinghc <xinghc@gmx.us>
2017-03-07 01:40:55 +0800

    An App to operate GET action

---
+++ WgetApp/WgetApp/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WgetApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            var uri = new Uri("https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data");
+            var client = new HttpClient();
+
+            textBlock.Text = "Fetching the data ...";
+            var messageDialog = new MessageDialog("Target: " + uri.ToString());
+            await messageDialog.ShowAsync();
+
+            var opContent = (await client.GetAsync(uri)).Content;           
+            string fileContents = await opContent.ReadAsStringAsync();
+            textBlock.Text = fileContents;
+        }
+    }
+}
