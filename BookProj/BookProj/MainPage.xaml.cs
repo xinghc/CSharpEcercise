c809ab609ff290f28c455c8a6f89c623ae70cdd8
xinghc <xinghc@gmx.us>
2017-03-23 00:36:37 +0800

    Added username password, and button click event

--- BookProj/BookProj/MainPage.xaml.cs
+++ BookProj/BookProj/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,23 @@ namespace BookProj
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void btOk_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlock tbkRes = new TextBlock();
+            tbkRes.Width = 350;
+            tbkRes.Height = 80;
+
+            SolidColorBrush scb = new SolidColorBrush();
+            scb.Color = Colors.Red;
+
+            tbkRes.Foreground = scb;
+            tbkRes.FontSize = 20;
+            tbkRes.Text = "Username = " + tbName.Text + "\n" +
+                          "Password = " + pbPassWd.Password;
+            tbkRes.Margin = new Thickness(0, 170, 5, 0);
+            ContentPanel.Children.Add(tbkRes);
         }
     }
 }
