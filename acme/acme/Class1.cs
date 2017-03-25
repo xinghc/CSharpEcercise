19210c4a75b8ad94f8463e9a49740502f5281925
xinghc <xinghc@gmx.us>
2017-03-25 17:24:36 +0800

    Stack operation with dll lib

---
+++ acme/acme/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Collections
+{
+    public class Stack
+    {
+        Entry top;
+        public void Push(object data)
+        {
+            top = new Entry(top, data);
+        }
+        public object Pop()
+        {
+            if (top == null) throw new InvalidOperationException();
+            object result = top.data;
+            top = top.next;
+            return result;
+        }
+        class Entry
+        {
+            public Entry next;
+            public object data;
+            public Entry(Entry next, object data)
+            {
+                this.next = next;
+                this.data = data;
+            }
+        }
+    }
+}
---
+++ acme_test/acme_test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Acme.Collections;
+
+namespace acme_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            s.Push(1);
+            s.Push(14);
+            s.Push(290);
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Pop());
+            Console.Read();
+        }
+    }
+}
