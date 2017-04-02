2cd736b23fcabfd85c57fde9f9839c3ef2405f3d
xinghc <xinghc@gmx.us>
2017-04-02 15:57:57 +0800

    Initial the exercise for SQLite (original from http://blog.tigrangasparian.com/2012/02/09/getting-started-with-sqlite-in-c-part-one/)

---
+++ SQLiteExe/SQLiteExe/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLiteConnection.CreateFile("MyDatabase.sqlite");
+            SQLiteConnection m_dbConnection;
+
+            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+            string sql = "create table highscores (name varchar(20), score int)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (name, score) values ('Me', 3000)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (name, score) values ('Myself', 6000)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (name, score) values ('And I', 9001)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            sql = "select * from highscores order by score desc";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+
+            m_dbConnection.Close();
+            Console.ReadLine();
+        }
+    }
+}
